da7dfed745fdb8980ea78c4f00a667829809c8f9
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2022-04-12 01:25:20 -0500

    notes route displays notes.html

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 const express = require('express');
 const path = require('path');
-const notesData = require('./db/db.json');
 
-const PORT = 5001
+const PORT = 3001
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.json);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'develop/public/notes.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
