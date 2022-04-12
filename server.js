cb7c256fb6a6d0f00af395b20eeaa8ebe7be09ce
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2022-04-12 01:27:49 -0500

    Root route connected to index.html

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.json);
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'develop/public/index.html')));
 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'develop/public/notes.html'))
