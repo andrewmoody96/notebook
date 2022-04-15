521b6d8eb4d7ce99a9f3c3972162d472f3d6eab7
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2022-04-15 15:38:44 -0500

    Made the ID generator prettier.

--- Develop/helpers/idGen.js
+++ Develop/helpers/idGen.js
@@ -1,7 +1,9 @@
 // Creates an ID by generating a random string of letters and numbers.
 
 const idgen = () => {
-    Math.floor((Math.random() * 0x1000)).toString(8).substring(1);
-}
+  Math.floor(Math.random() * 0x1000)
+    .toString(8)
+    .substring(1);
+};
 
 module.exports = idgen;
