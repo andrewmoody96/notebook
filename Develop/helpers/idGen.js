1c756c75ba5b8cbb219c6314d6b47817847d0648
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2022-04-15 15:38:27 -0500

    Created a note ID generator.

---
+++ Develop/helpers/idGen.js
@@ -0,0 +1,7 @@
+// Creates an ID by generating a random string of letters and numbers.
+
+const idgen = () => {
+    Math.floor((Math.random() * 0x1000)).toString(8).substring(1);
+}
+
+module.exports = idgen;
