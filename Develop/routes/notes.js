795356de2be26d9e50d1ba951f019185d9cdb85e
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2022-04-15 21:29:26 -0500

    Added requirement for idGen & created post request.

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const notes = express.Router();
 const db = require('../db/db.json');
+const idGen = require('../helpers/idGen');
 
 
 notes.get('/', (req, res) => {
     console.log('Request Received');
     readFromFile('./db/db.json'.then((data) => res.json(JSON.parse(data))));
 });
+
+notes.post('/', (req, res) => {
+    console.log('Adding note');
+    const { title, text } = req.body;
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            note_id: idGen(),
+        };
+        readAndAppend(newNote, './db/db.json');
+        const response = {
+            status: 'Saved',
+            body: newNote,
+        };
+        res.json(response);
+    } else {
+        res.json('Error while trying to save note.');
+    }
+});
+
 
 module.exports = notes;
