e13a19b01040e0119827ad5b8bb6e8a4677bff3d
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2022-04-17 00:16:11 -0500

    changed ID function to add 1

--- helpers/idGen.js
+++ helpers/idGen.js
@@ -1,6 +1,6 @@
 // Creates an ID by generating a random string of letters and numbers.
 
 module.exports = () => 
-  Math.floor(Math.random() * 0x1000)
-    .toString(5)
+  Math.floor((1 + Math.random()) * 0x1000)
+    .toString(16)
     .substring(1);
