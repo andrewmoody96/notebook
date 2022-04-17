bfe4429e9109a9ec5ed17f97caa6295d93d4cc27
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2022-04-16 22:50:59 -0500

    Trying to deploy to Heroku.

--- helpers/idGen.js
+++ helpers/idGen.js
@@ -1,9 +1,6 @@
 // Creates an ID by generating a random string of letters and numbers.
 
-const idGen = () => {
+module.exports = () => 
   Math.floor(Math.random() * 0x1000)
     .toString(5)
     .substring(1);
-};
-
-module.exports = idGen;
